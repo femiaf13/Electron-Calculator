ca3168ce63fede37dbd22f8359196768b7c319de
Frank Femia <femiaf13@gmail.com>
2018-11-20 14:32:10 -0500

    Refactored calc.ts into a class

--- src/calc.ts
+++ src/calc.ts
@@ -2,82 +2,123 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-let value: number = 0;
-// on deck value that will affect value
-let on_deck: string = '';
-let operation: string = 'none'
+class Calculator {
+    answer: number;
+    // on deck value that will affect value
+    on_deck: string;
+    // 3 character string for which operation to perform
+    operation: string;
 
-function math(event: MouseEvent): void {
-    switch(operation) {
-        case 'add':
-            value += parseFloat(on_deck);
-            break;
-        case 'sub':
-            value -= parseFloat(on_deck);
-            break;
-        case 'mul':
-            value *= parseFloat(on_deck);
-            break;
-        case 'div':
-            value /= parseFloat(on_deck);
-            break;
-        default:
-            break;
+    button_1: HTMLButtonElement;
+    button_2: HTMLButtonElement;
+    button_3: HTMLButtonElement;
+    button_4: HTMLButtonElement;
+    button_5: HTMLButtonElement;
+    button_6: HTMLButtonElement;
+    button_7: HTMLButtonElement;
+    button_8: HTMLButtonElement;
+    button_9: HTMLButtonElement;
+    button_0: HTMLButtonElement;
+    button_add: HTMLButtonElement;
+    button_sub: HTMLButtonElement;
+    button_mul: HTMLButtonElement;
+    button_div: HTMLButtonElement;
+    button_equal: HTMLButtonElement;
+    button_clear: HTMLButtonElement;
+
+    constructor() {
+        this.answer = 0;
+        this.on_deck = '';
+        this.operation = 'nul'
+
+        this.button_1 = document.getElementById('button_1') as HTMLButtonElement;
+        this.button_2 = document.getElementById('button_2') as HTMLButtonElement;
+        this.button_3 = document.getElementById('button_3') as HTMLButtonElement;
+        this.button_4 = document.getElementById('button_4') as HTMLButtonElement;
+        this.button_5 = document.getElementById('button_5') as HTMLButtonElement;
+        this.button_6 = document.getElementById('button_6') as HTMLButtonElement;
+        this.button_7 = document.getElementById('button_7') as HTMLButtonElement;
+        this.button_8 = document.getElementById('button_8') as HTMLButtonElement;
+        this.button_9 = document.getElementById('button_9') as HTMLButtonElement;
+        this.button_0 = document.getElementById('button_0') as HTMLButtonElement;
+        this.button_add = document.getElementById('button_add') as HTMLButtonElement;
+        this.button_sub = document.getElementById('button_sub') as HTMLButtonElement;
+        this.button_mul = document.getElementById('button_mul') as HTMLButtonElement;
+        this.button_div = document.getElementById('button_div') as HTMLButtonElement;
+        this.button_equal = document.getElementById('button_equal') as HTMLButtonElement;
+        this.button_clear = document.getElementById('button_clear') as HTMLButtonElement;
+
+        var num_pad: Array<HTMLButtonElement> = [this.button_0, this.button_1, this.button_2,
+            this.button_3, this.button_4, this.button_5, this.button_6, this.button_7, 
+            this.button_8, this.button_9]
+    
+        var operators: Array<HTMLButtonElement> = [this.button_add, this.button_sub,
+        this.button_mul, this.button_div]
+
+        // add event listeners
+        num_pad.forEach(el => el.addEventListener('click', () => this.input(event)));
+        operators.forEach(el => el.addEventListener('click', () => this.operator(event)));
+        // using an arrow function because it preserves "this" in whatever you call
+        this.button_equal.addEventListener('click', () => this.math(event));
     }
-    // reset operation to none so you can't keep doing operations
-    operation = 'none';
-    on_deck = '';
-    document.getElementById('equals').innerHTML = '='+value.toString();
-    console.log('Value: '+value.toString());
+
+    input(event: Event): void {
+        var button: HTMLButtonElement = event.target as HTMLButtonElement;
+        this.on_deck += button.value;
+
+        if (this.operation == 'nul') {
+            document.getElementById('left').innerHTML = this.on_deck;
+        }
+        else {
+            document.getElementById('right').innerHTML = this.on_deck;
+        } 
+    }
+
+    operator(event: Event): void {
+        var button: HTMLButtonElement = event.target as HTMLButtonElement;
+        // reset the equation
+        document.getElementById('right').innerHTML = '';
+        document.getElementById('equals').innerHTML = '';
+        // Don't convert value into empty string
+        if (this.on_deck !== ''){
+            // Store on deck value into value
+            // This is for the "left hand" number
+            this.answer = parseFloat(this.on_deck);
+            document.getElementById('left').innerHTML = this.on_deck;
+        }
+        else{
+            // Operating on previous calculated value, move it to the left
+            document.getElementById('left').innerHTML = this.answer.toString();
+        }
+        this.on_deck = '';
+        this.operation = button.value;
+        document.getElementById('operation').innerHTML = button.innerHTML;
+    }
+
+    math(event: Event): void {
+        var button: HTMLButtonElement = event.target as HTMLButtonElement;
+        switch(this.operation) {
+            case 'add':
+                this.answer += parseFloat(this.on_deck);
+                break;
+            case 'sub':
+                this.answer -= parseFloat(this.on_deck);
+                break;
+            case 'mul':
+                this.answer *= parseFloat(this.on_deck);
+                break;
+            case 'div':
+                this.answer /= parseFloat(this.on_deck);
+                break;
+            default:
+                break;
+        }
+        // reset operation to none so you can't keep doing operations
+        this.operation = 'nul';
+        this.on_deck = '';
+        document.getElementById('equals').innerHTML = '='+this.answer.toString();
+        console.log('Value: '+this.answer.toString());
+    }
 }
 
-function input(event: MouseEvent): void {
-    // Whoever calls this function is the button being pressed
-    on_deck += this.value
-
-    if (operation == 'none') {
-        document.getElementById('left').innerHTML = on_deck;
-    }
-    else {
-        document.getElementById('right').innerHTML = on_deck;
-    }
-}
-
-function operator(event: MouseEvent): void {
-    // reset the equation
-    document.getElementById('right').innerHTML = '';
-    document.getElementById('equals').innerHTML = '';
-    // Don't convert value into empty string
-    if (on_deck !== ''){
-        // Store on deck value into value
-        // This is for the "left hand" number
-        value = parseFloat(on_deck);
-        document.getElementById('left').innerHTML = on_deck;
-    }
-    else{
-        // Operating on previous calculated value, move it to the left
-        document.getElementById('left').innerHTML = value.toString();
-    }
-    on_deck = '';
-    operation = this.value;
-    document.getElementById('operation').innerHTML = this.innerHTML;
-}
-
-// TODO: Add way to clear
-//       Group these listeners in cleaner way
-
-document.getElementById('button_1').addEventListener("click", input);
-document.getElementById('button_2').addEventListener("click", input);
-document.getElementById('button_3').addEventListener("click", input);
-document.getElementById('button_4').addEventListener("click", input);
-document.getElementById('button_5').addEventListener("click", input);
-document.getElementById('button_6').addEventListener("click", input);
-document.getElementById('button_7').addEventListener("click", input);
-document.getElementById('button_8').addEventListener("click", input);
-document.getElementById('button_9').addEventListener("click", input);
-document.getElementById('button_0').addEventListener("click", input);
-document.getElementById('button_add').addEventListener("click", operator);
-document.getElementById('button_sub').addEventListener("click", operator);
-document.getElementById('button_mul').addEventListener("click", operator);
-document.getElementById('button_div').addEventListener("click", operator);
-document.getElementById('button_equal').addEventListener("click", math);
+let calc: Calculator = new Calculator();
